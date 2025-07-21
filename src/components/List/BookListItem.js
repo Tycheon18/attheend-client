218a6db8e2f247332bd4198035b9949d5a8880e3
Tycheon18 <joong0818@gmail.com>
2025-07-22 02:47:56 +0900

    Add basic CSS for BookListItem.

--- src/components/List/BookListItem.js
+++ src/components/List/BookListItem.js
@@ -1,18 +1,29 @@
 import styles from "./BookListItem.module.css"
+import "./BookList.module.css";
 
 const BookListItem = ({ book }) => {
+    
+    const content = book.contents;
+    const length = content.length;
+    console.log(length);
 
     return (
         <article className={styles.BookListItem}>
             <div className={styles.thumbnail_wrapper}>
-                <img src={book.thumbnail} alt={`표지: ${book.title}`} />
+                <img className={styles.cover} src={book.thumbnail} alt={`표지: ${book.title}`} />
             </div>
-            <div className={styles.title_wrapper}>
-                {book.title}
+            <div>
+                <h1 className={styles.title}>
+                    {book.title}
+                </h1>
+                <h2 className={styles.author}>
+                    {book.authors.join(", ")}
+                </h2>
+                <p>
+                    {book.contents.slice(0, 150) + "..."}
+                </p>
             </div>
-            <div className={styles.authros_wrapper}>
-                {book.authors.join(", ")}
-            </div>
+
         </article>
     )
 }
